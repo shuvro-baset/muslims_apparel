ce7577d05747323b5121db13c403a34514057d60
shuvro baset <md.abdul.baset75@gmail.com>
2021-08-24 22:56:42 +0600

    ngrok added

--- muslims_apparel/settings.py
+++ muslims_apparel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7g0l_jtbgp17(cpia2qg)#2n_$(*r%i)h3_+6-a*uox+e1edn%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
