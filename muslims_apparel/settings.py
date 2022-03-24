fdac166d12e6f57e0a85d1de3783be700ba52a2e
shuvro-baset <md.abdul.baset75@gmail.com>
2022-03-24 23:53:52 +0600

    restructure wip

--- muslims_apparel/settings.py
+++ muslims_apparel/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default='*')
-
+# ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default='*')
+ALLOWED_HOSTS = []
 MODE = env.str('MODE', default='production')
 
 # SECURITY WARNING: keep the secret key used in production secret!
